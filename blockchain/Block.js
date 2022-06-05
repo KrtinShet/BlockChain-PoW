61a7a51d45719e595c46fe45516085926be76ca8
Krtin Shet <krtinshet.dev@gmail.com>
2022-06-05 23:00:41 +0530

    blockchain with mining added

--- blockchain/Block.js
+++ blockchain/Block.js
@@ -1,25 +1,49 @@
 const cryptoHash = require("../utils/cryptoHash");
-const { GENESIS_BLOCK_DATA } = require("./../config");
+const { GENESIS_BLOCK_DATA, MINE_RATE } = require("./../config");
+const hexToBinary = require("hex-to-binary");
+
 class Block {
-  constructor({ timestamp, lastHash, hash, data }) {
+  constructor({ timestamp, lastHash, hash, data, nonce, difficulty }) {
     this.timestamp = timestamp;
     this.lastHash = lastHash;
     this.hash = hash;
     this.data = data;
+    this.nonce = nonce;
+    this.difficulty = difficulty;
   }
   static genesis() {
     return new Block(GENESIS_BLOCK_DATA);
   }
 
   static mineBlock({ lastBlock, data }) {
-    const timestamp = Date.now();
     const lastHash = lastBlock.hash;
-    return new this({
-      timestamp,
-      data,
-      hash: cryptoHash(timestamp, lastHash, data),
-      lastHash,
-    });
+    let hash, timestamp;
+    let { difficulty } = lastBlock;
+    let nonce = 0;
+
+    do {
+      nonce++;
+      timestamp = Date.now();
+      difficulty = Block.adjustDifficulty({
+        originalBlock: lastBlock,
+        timestamp,
+      });
+      hash = cryptoHash(timestamp, lastHash, data, nonce, difficulty);
+    } while (
+      hexToBinary(hash).substring(0, difficulty) !== "0".repeat(difficulty)
+    );
+
+    return new this({ timestamp, lastHash, data, difficulty, nonce, hash });
+  }
+
+  static adjustDifficulty({ originalBlock, timestamp }) {
+    const { difficulty } = originalBlock;
+
+    if (difficulty < 1) return 1;
+
+    if (timestamp - originalBlock.timestamp > MINE_RATE) return difficulty - 1;
+
+    return difficulty + 1;
   }
 }
 
--- config.js
+++ config.js
@@ -1,16 +1,17 @@
-const GENESIS_BLOCK_DATA = {
-  timestamp: 1,
-  lastHash: "-----",
-  hash: "hash-one",
-  data: [],
-};
-
 const MINE_RATE = 1000;
 const INITIAL_DIFFICULTY = 3;
 
 const STARTING_BALANCE = 1000;
 
 const REWARD_INPUT = { address: "*authorized-reward*" };
+const GENESIS_BLOCK_DATA = {
+  timestamp: 1,
+  lastHash: "-----",
+  hash: "hash-one",
+  difficulty: INITIAL_DIFFICULTY,
+  nonce: 0,
+  data: [],
+};
 
 const MINING_REWARD = 50;
 
